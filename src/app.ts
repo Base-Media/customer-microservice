698f61d678923f05fe2337733e839c94fe5eac30
Claude-Lamarre99 <161344701+Claude-Lamarre99@users.noreply.github.com>
2024-11-22 14:51:40 -0500

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 /** @format */
 
 import express from 'express';
+import cors from 'cors';
 import customerRoutes from './api/customerRoutes';
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use('/api', customerRoutes);
 
